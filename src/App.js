221d6ab275a8e96160df99ed1405b11a1b7b35b2
Gyeongjin Kim <kj4483@naver.com>
2024-02-03 17:06:26 +0900

    #7.2 done
    React practice with simple Coin Viewer

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
 import { useState, useEffect } from "react";
 
 function App() {
-    const [todo, setTodo] = useState("");
-    const [todos, setTodos] = useState([]);
-    const onChange = (event) => setTodo(event.target.value);
-    const onSubmit = (event) => {
-        event.preventDefault();
-        if (todo === "") return;
-        setTodo("");
-        setTodos((currentArray) => [todo, ...currentArray]);
-    };
-    console.log(todos);
+    const [loading, setLoading] = useState(true);
+    const [coins, setCoins] = useState([]);
+    useEffect(() => {
+        fetch("https://api.coinpaprika.com/v1/tickers")
+            .then((response) => response.json())
+            .then((json) => {
+                setCoins(json);
+                setLoading(false);
+            });
+    }, []);
     return (
         <div>
-            <h1>my to dos ({todos.length})</h1>
-            <form onSubmit={onSubmit}>
-                <input
-                    onChange={onChange}
-                    value={todo}
-                    type="text"
-                    placeholder="Write your to do..."
-                />
-                <button>Add To Do</button>
-            </form>
+            <h1>Coin Viewer! {loading ? "" : `(${coins.length})`}</h1>
             <hr />
-            <ul>
-                {todos.map((item, index) => (
-                    <li key={index}>{item}</li>
-                ))}
-            </ul>
+            {loading ? (
+                <strong>Loading...</strong>
+            ) : (
+                <ul>
+                    {coins.map((coin) => (
+                        <li>
+                            {coin.name} ({coin.symbol}): {coin.quotes.USD.price}{" "}
+                            USD
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 }
