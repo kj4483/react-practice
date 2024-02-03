8b82bbe6d605672eefdba631c356d8042b2067bf
Gyeongjin Kim <kj4483@naver.com>
2024-02-03 16:25:20 +0900

    #7.1 done
    React practice with simple todo list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+    const [todo, setTodo] = useState("");
+    const [todos, setTodos] = useState([]);
+    const onChange = (event) => setTodo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (todo === "") return;
+        setTodo("");
+        setTodos((currentArray) => [todo, ...currentArray]);
+    };
+    console.log(todos);
     return (
         <div>
-            <h1 className={styles.title}>Welcome!</h1>
-            <Button text="asdfjk;" />
+            <h1>my to dos ({todos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input
+                    onChange={onChange}
+                    value={todo}
+                    type="text"
+                    placeholder="Write your to do..."
+                />
+                <button>Add To Do</button>
+            </form>
+            <hr />
+            <ul>
+                {todos.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
         </div>
     );
 }
