00ca2f8ec8d0796dbc7ff914a4b831d53823d95a
Kim Gyeongjin <kj4483@kakao.com>
2024-02-05 03:03:50 +0900

    #7.5 in progress
    Fix new react-router-dom release Route script

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
+// import { useState } from "react";
 
 function App() {
+    // const [showing, setShowing] = useState(false);
+    // const onClick = () => setShowing((prev) => !prev);
     return (
         <Router>
             <Routes>
-                <Route path="/">
-                    <Home />
-                </Route>
+                <Route path="/" element={<Home />} />
             </Routes>
         </Router>
     );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,19 +4,16 @@ import Movie from "../components/Movie";
 function Home() {
     const [loading, setLoading] = useState(true);
     const [movies, setMovies] = useState([]);
-
-    useEffect(() => {
-        // eslint-disable-next-line
-        const getMovie = async () => {
-            const json = await (
-                await fetch(
-                    "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5%sort_by=year"
-                )
-            ).json();
-            setMovies(json.data.movies);
-            setLoading(false);
-        };
-    }, []);
+    const getMovies = async () => {
+        const json = await (
+            await fetch(
+                "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5%sort_by=year"
+            )
+        ).json();
+        setMovies(json.data.movies);
+        setLoading(false);
+    };
+    useEffect(getMovies, []);
     return (
         <div>
             {loading ? (
